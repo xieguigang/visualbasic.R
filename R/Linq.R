8ca221cedf17e86b46a3318fac3e793f4c7ebe51
谢桂纲 <xie.guigang@live.com>
2017-10-12 14:55:49 +0800

    group by

--- R/Linq.R
+++ R/Linq.R
@@ -1,3 +1,62 @@
-Take <- function(collection, m) {
+Take <- function(enumerable, m) {
 
+}
+
+# group data.frame/list by keys
+#
+# @param enumerable The data source collection for the group operation, by default is ``data.frame`` type.
+# @param key The column name or property name for using its corresponding value as the group key
+# @param type Indicate that the data source ``enumerable`` is a data.frame or list? default is a data.frame.
+GroupBy <- function(enumerable, key, type = c("data.frame", "list")) {
+
+    if (type == "data.frame") {
+        GroupBy.dataframe(data.frame = enumerable, key = key);
+    } else if (type == "list") {
+        GroupBy.list(list = enumerable, key = key);
+    } else {
+        stop("Not supported!");
+    }
+
+}
+
+# Groups the items in a list by s specific property of the list item.
+#
+# @param list The data source in list type
+# @param key The item property name in this list source collection.
+GroupBy.list <- function(list, key) {
+    
+    groups <- list();
+
+    for (i in 1:length(list)) {
+        item <- list[[i]];
+        group.key <- item[[key]];
+        key.list <- groups[[group.key]];
+        
+        if (is.null(key.list)) {
+            key.list <- list();
+        }
+
+        key.list[[length(key.list) + 1]] <- item;
+        groups[[group.key]] <- key.list;
+    }
+
+    return(groups);
+}
+
+# Groups the rows in a dataframe by a specific column as key.
+#
+# @param data.frame The data source in data.frame type
+# @param key The column name for read the column data in target source as the group key.
+GroupBy.dataframe <- function(data.frame, key) {
+    
+    groups      <- list();
+    key.index   <- which(colnames(data.frame) == key);
+
+    for (i in 1:nrow(data.frame)) {
+        row <- data.frame[i, ];
+        key <- row[key.index];
+        groups[key] <- rbind(group[[key]], row);
+    }
+
+    return(groups);
 }
