d48374ad9886fc02506952f619d79cf44a00e986
xieguigang <xie.guigang@gcmodeller.org>
2017-11-09 10:06:53 +0800

    add text file write

--- R/File.R
+++ R/File.R
@@ -7,3 +7,15 @@ basename <- function(path) {
 	
 	return(file)
 }
+
+# 向目标指定的文件中追加指定的文本数据
+Println <- function(file.txt, content) {
+
+	cat(content, file = file.txt, append = TRUE);
+	cat("\n",    file = file.txt, append = TRUE);
+	
+}
+
+File.Open <- function(file.txt) {
+	         function(content) Println(file.txt, content);
+}
---
+++ R/XML.R
@@ -0,0 +1,26 @@
+# 将任意的R对象序列化为XML文件保存
+
+GetXML <- function(x) {
+
+    xml <- tempfile("xml-");
+
+    if (is.list(x)) {
+
+        # 是一个类似于字典对象的东西
+
+    } else {
+
+        # 是相同元素的vector
+
+    }
+
+}
+
+Vector.XML <- function(vector) {
+
+}
+
+List.XML <- function(vector) {
+
+}
+
