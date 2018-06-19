15bad95c90917a127b4c3c4799e02d2a09d97223
xieguigang <xie.guigang@gcmodeller.org>
2018-06-19 10:36:42 +0800

    add benchmark test

--- test/binarysearchTest.R
+++ test/binarysearchTest.R
@@ -1,11 +1,50 @@
 d <- read.csv("./identify.csv");
 
-binarySearch(d, 516, "mz", function(a, b) {
-	if (abs(a-b) <= 0.3) {
-		0;
-	} else if ( a < b) {
-		-1;
+tolerance <- function(a, b) {
+	abs.d <- abs(a-b);	
+	
+	if (length(a) > 1) {
+		sapply(1:length(abs.d), function(i) {
+			d <- abs.d[i];
+					
+			if (d <= 0.3) {
+				0;
+			} else if (a[i] < b) {
+				-1;
+			} else {
+				1;
+			}
+		});
 	} else {
-		1;
+		if (abs.d <= 0.3) {
+			0;
+		} else if (a < b) {
+			-1;
+		} else {
+			1;
+		}
 	}
-});
+}
+
+binarySearch(d, 516, "mz", tolerance);
+
+# 二叉树查找在小规模的数据集上面性能区分不大
+# 但是在大型数据集上面非常有效
+
+benchmark.b <- function() {
+	system.time({
+		x <- binarySearch(d, 516, "mz", tolerance);	
+		
+		print("Search by binary search");
+		print(x);
+	});
+}
+
+benchmark.n <- function() {
+	system.time({
+		x <- which(tolerance(as.vector(d[, "mz"]), 516) == 0);
+		
+		print("Search by which");
+		print(d[x, ]);
+	});
+}
