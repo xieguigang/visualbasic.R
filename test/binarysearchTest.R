36685bd45300d62955e4a711a611725246970804
xieguigang <xie.guigang@gcmodeller.org>
2018-06-19 10:16:55 +0800

    torrelance test success

--- test/binarysearchTest.R
+++ test/binarysearchTest.R
@@ -1,3 +1,11 @@
 d <- read.csv("./identify.csv");
 
-binarySearch(d, 234.1082192, "mz");
+binarySearch(d, 516, "mz", function(a, b) {
+	if (abs(a-b) <= 0.3) {
+		0;
+	} else if ( a < b) {
+		-1;
+	} else {
+		1;
+	}
+});
