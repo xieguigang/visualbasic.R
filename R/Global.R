875985e60eed9cce1489ee8b4776fe6646d2fa5f
xieguigang <xie.guigang@gcmodeller.org>
2018-06-21 16:39:28 +0800

    add code comments

--- R/Global.R
+++ R/Global.R
@@ -1,9 +1,20 @@
-imports <- function(namespace, overrides = FALSE) {
+#' VisualBasic namespace imports helper
+#'
+#' @param namespace The namespace function name
+#' @param overrides A logical flag to indicate that should the imported function
+#'                  can overrides the previous function which they have the same
+#'                  name. By default is can not. If this parameter is set to true,
+#'                  then a warning message will be generated for mention you that
+#'                  which functions are overrided.
+#'
+#' @return A string vector contains all of the function names from this namespace
+#'         function that imported to current environment.
+imports <- function(namespace, overrides = FALSE, silent = TRUE) {
 	frame       <- parent.frame();
 	module      <- get(namespace, envir = frame);
 	func.list   <- module();
 	overrideMsg <- "overrides '%s' from namespace `%s`";
-	
+
 	for (name in names(func.list)) {
 		if (exists(name, envir = frame)) {
 			if (overrides) {
@@ -12,21 +23,32 @@ imports <- function(namespace, overrides = FALSE) {
 				next;
 			}
 		}
-		
+
 		assign <- list(name, func.list[[name]]);
 		do.call(`=`, assign, envir = frame);
 	}
-	
+
 	# invisible(NULL);
-	names(func.list);
+	if (silent) {
+	  invisible(NULL);
+	} else {
+	  names(func.list);
+	}
 }
 
-# 判断对象是否为空
+#' Determine that target \code{is.null} or \code{is.na} or \code{length}
+#' equals to ZERO?
+#'
+#' @param x. Object with any type for determine that its value is null
+#'           or empty or not?
+#'
+#' @details 判断对象是否为空，在这个函数里面，空值，NA值，长度为零的向量，
+#'          列表等都会被当作为空值
 IsNothing <- function(x.) is.null(x.) || is.na(x.) || length(x.) == 0;
-# 模拟C语言的打印函数
+#' 模拟C语言的打印函数
 printf <- function(...) invisible(print(sprintf(...)));
 
-# syntax tweaks
+#' syntax tweaks
 microsoft.visualbasic.language <- function() {
 
 	# 如果对象为空则取默认值
@@ -34,14 +56,14 @@ microsoft.visualbasic.language <- function() {
 
 	## @param path: list(obj, "/path/ref");
 	`%<=%` <- function(path, value) {
-		
+
 		# get parent environment
 		.globalEnvir <- parent.frame();
-		
+
 		# set an invisible temp variable
-		t        = "tmp_0x2C34B_assign_Helper"; 
+		t        = "tmp_0x2C34B_assign_Helper";
 		do.call(`=`, list(t, value), envir = .globalEnvir);
-		
+
 		x       <- path[[1]];
 		x.value <- get(x);
 		path    <- Strings.Split(path[[2]], "/");
@@ -53,8 +75,8 @@ microsoft.visualbasic.language <- function() {
 
 		# removes the temp helper variable
 		do.call(`rm`, list(t), envir = .globalEnvir);
-		
-		return(invisible(NULL)) 
+
+		return(invisible(NULL))
 	}
 
 	# tuple syntax helper
@@ -75,38 +97,38 @@ microsoft.visualbasic.language <- function() {
 	':=' <- function(lhs, rhs) {
 		frame <- parent.frame();
 		lhs   <- as.list(substitute(lhs))
-		
+
 		if (length(lhs) > 1)
 			lhs <- lhs[-1];
 		if (length(lhs) == 1) {
 			do.call(`=`, list(lhs[[1]], rhs), envir=frame)
 			return(invisible(NULL));
 		}
-		
+
 		if (is.function(rhs) || is(rhs, 'formula'))
 			rhs <- list(rhs);
 		if (length(lhs) > length(rhs))
 			rhs <- c(rhs, rep(list(NULL), length(lhs) - length(rhs)));
-			
+
 		for (i in 1:length(lhs))
 			do.call(`=`, list(lhs[[i]], rhs[[i]]), envir=frame);
-		
+
 		invisible(NULL);
 	}
-	
+
 	# Extension method operator
-	`%=>%` <- function(x, y) { 
-		y(x);   
+	`%=>%` <- function(x, y) {
+		y(x);
 	}
-	
-	list("%||%" = get("%||%"), 
-		 "%<=%" = get("%<=%"), 
+
+	list("%||%" = get("%||%"),
+		 "%<=%" = get("%<=%"),
 		 ":="   = get(":="),
-		 "%=>%" = get("%=>%")		 
+		 "%=>%" = get("%=>%")
 	);
 }
 
-## 函数返回primitiveTypes枚举之中的某一个类型
+#' 函数返回\code{\link{primitiveTypes}}枚举之中的某一个类型
 GetType <- function(x) {
 	types <- primitiveTypes();
 
@@ -121,8 +143,13 @@ GetType <- function(x) {
 	}
 }
 
-## 枚举出R语言之中的一些基础的数据类型，枚举值有：
-## object, data.frame, list, vector
+#' 枚举出R语言之中的一些基础的数据类型，枚举值有：
+#' \enumerate{
+#' \item \code{object} = 0
+#' \item \code{data.frame} = 1
+#' \item \code{list} = 2
+#' \item \code{vector} = 3
+#' }
 primitiveTypes <- function() {
 	list(object = 0, data.frame = 1, list = 2, vector = 3);
 }
---
+++ R/binaryTree.R
@@ -0,0 +1,14 @@
+
+#' This function build a binary tree object
+#'
+#' @param src the input data sequence, which can be \code{data.frame}, \code{list} or
+#'            \code{vector}
+#' @param key An object indexer for get the key value for the elements
+#' @param key.numeric Convert the key value to numeric value for comparasions.
+#'
+#' @return A binary tree S4 class object
+binaryTree <- function(src, key, key.numeric = as.numeric) {
+
+}
+
+
