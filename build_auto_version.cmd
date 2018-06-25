a12a81fe326947f309e1435bcda9f1e91604c263
xieguigang <xie.guigang@gcmodeller.org>
2018-06-25 17:48:01 +0800

    auto version builder script

--- build_auto_version.cmd
+++ build_auto_version.cmd
@@ -1,4 +1,10 @@
 @echo off
 
 SET compiler=%1
+SET auto_ver="Utils\App\AutoVersion.exe"
 
+REM please do not modify DESCRIPTION directly, modify DESCRIPTION.template 
+REM file at first, and then DESCRIPTION file will be update automatically
+%auto_ver% DESCRIPTION.template > DESCRIPTION
+
+CALL "build.bat" %compiler%
