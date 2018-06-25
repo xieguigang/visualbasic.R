2a8225ca4abe550c3a4ed887515a92df64ca13a6
xieguigang <xie.guigang@gcmodeller.org>
2018-06-25 17:59:59 +0800

    update version and save value

--- Utils/AutoVersion/AutoVersion/Program.vb
+++ Utils/AutoVersion/AutoVersion/Program.vb
@@ -1,9 +1,28 @@
 ﻿Imports System.IO
+Imports Microsoft.VisualBasic.Language
 
 Module Program
 
     Sub Main()
         Dim template$ = App.CommandLine.Name
+        Dim version As Value(Of String) = template.ParentPath & "/version.txt"
+        Dim ver#() = version.Value _
+            .ReadAllText _
+            .Split("."c) _
+            .Select(AddressOf Val) _
+            .ToArray
+
+        For i As Integer = ver.Length - 1 To 0 Step -1
+            If i > 0 AndAlso ver(i) = 999 Then
+                ver(i) = 0
+                ver(i - 1) += 1
+            End If
+        Next
+
+        With CType(version, String)
+            Call (version = ver.JoinBy(".")).SaveTo(.ByRef)
+        End With
+
         Dim description = template _
             .IterateAllLines _
             .Where(Function(l) Not l.StringEmpty) _
@@ -14,19 +33,7 @@ Module Program
         With New StringWriter(Console.OpenStandardOutput)
             For Each tuple In description
                 If tuple.Name.TextEquals("version") Then
-                    Dim ver#() = tuple.Value _
-                        .Split("."c) _
-                        .Select(AddressOf Val) _
-                        .ToArray
-
-                    For i As Integer = ver.Length - 1 To 0 Step -1
-                        If i > 0 AndAlso ver(i) = 999 Then
-                            ver(i) = 0
-                            ver(i - 1) += 1
-                        End If
-                    Next
-
-                    Call .WriteLine($"Version: {ver.JoinBy(".")}")
+                    Call .WriteLine($"Version: {version}")
                 ElseIf tuple.Name.StringEmpty Then
                     Call .WriteLine($"  {tuple.Value}")
                 Else
