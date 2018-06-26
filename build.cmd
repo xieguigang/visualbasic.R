840929f089f6d4b99861ee9a8d8614441d921d7a
xieguigang <xie.guigang@gcmodeller.org>
2018-06-26 09:09:41 +0800

    fix namespace file overrides location

--- build.cmd
+++ build.cmd
@@ -11,7 +11,7 @@ REM echo %compiler%
 REM echo %default%
 
 REM overrides back the NAMESPACE file which is damaged by the roxygen2 package
-echo exportPattern("^[[:alpha:]]+") > NAMESPACE
+echo exportPattern("^[[:alpha:]]+") > "Rpackage\NAMESPACE"
 
 REM build R package
 REM By default the R compiler is located at "D:\R\bin\x64\Rcmd.exe"
