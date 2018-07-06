3adc0d540e9903de16cdd4de2f72e169bfce8a18
xieguigang <xie.guigang@gcmodeller.org>
2018-07-06 23:04:14 +0800

    parallel helpers

---
+++ Rpackage/R/parallelHelper.R
@@ -0,0 +1,35 @@
+getClusterCores <- function(level = c("full", "half", "parallel", "single")) {
+  if (IsNothing(level)) {
+    level = "full";
+  } else {
+    level = level[1] %=>% Strings.LCase;
+  }
+
+  require(parallel);
+
+  cores <- detectCores();
+
+  if (cores == 1) {
+    return(1);
+  }
+
+  if (level == "full") {
+    return(cores);
+  } else if (level == "half") {
+    set.cores <- cores / 2;
+  } else if (level == "parallel") {
+    set.cores <- cores / 4;
+  } else if (level == "single") {
+    return(1);
+  } else {
+    stop(sprintf("Unknown option: level='%s'", level));
+  }
+
+  if (set.cores < 1) {
+    set.cores <- 1;
+  } else {
+    set.cores <- round(set.cores);
+  }
+
+  set.cores;
+}
