de1650a40d0c869e983ff2669123a1a90794c036
xieguigang <xie.guigang@gcmodeller.org>
2018-07-20 15:17:30 +0800

    fix for code example

--- Rpackage/R/Helpers.R
+++ Rpackage/R/Helpers.R
@@ -4,11 +4,12 @@
 #' @param a variable 1
 #' @param b variable 2
 #'
-#' @example
+#' @details
+#'
+#' example as:
 #'
 #'    a <- 1;
 #'    b <- "Hello World!";
-#'
 #'    c(b, a) := swap(a, b);
 #'
 swap <- function(a, b) list(a = b, b = a);
