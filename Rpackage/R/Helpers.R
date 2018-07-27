d1b1ec04c26f7da6b578c0f8ba1061620fa17a0e
xieguigang <xie.guigang@gcmodeller.org>
2018-07-27 13:19:07 +0800

    add commandline parser

--- Rpackage/R/Helpers.R
+++ Rpackage/R/Helpers.R
@@ -22,3 +22,72 @@ user <- function() {
   cli = "echo \"echo $USER\" | bash";
   system(cli, intern = TRUE);
 }
+
+#' Get commandline
+#'
+#' @note
+#'
+#' Commandline parser for cli expression pattern like:
+#'
+#' \code{Rscript script.R /name /arg1 value1 /arg2 value2 /boolean1 /arg3 value3}
+#'
+argv <- function() {
+
+  cli <- commandArgs();
+
+  if (.Platform$OS.type == "windows") {
+    # commandline on windows
+    #
+    # [1] "D:\\R\\bin\\x64\\Rterm.exe"
+    # [2] "--slave"
+    # [3] "--no-restore"
+    # [4] "--file=D:\\smartnucl_integrative\\biodeepDB\\internal/Rscripts/mz_calculator.R"
+    # [5] "--args"
+    # [6] "-1"
+    # [7] "745.0911"
+    # [8] "./data/temp/mz_calculator_TMgNO9CQ"
+
+    cli <- cli[6:length(cli)];
+  } else {
+    # commandline on linux
+    #
+    # [1] "/usr/local/software/R-3.4.3/lib64/R/bin/exec/R"
+    # [2] "--slave"
+    # [3] "--no-restore"
+    # [4] "--file=./mz_calculator.R"
+    # [5] "--args"
+    # [6] "-1"
+    # [7] "745.0911"
+    # [8] "./mz_calculator_TMgNO9CQ"
+
+    cli <- cli[6:length(cli)];
+  }
+
+  name <- cli[1];
+  args <- list();
+  i    <- 2;
+  is.argName <- function(x) {
+    if (x %=>% IsNothing) {
+      return (FALSE);
+    }
+
+    base::startsWith(x, "/")  ||
+    base::startsWith(x, "--") ||
+    base::startsWith(x, "-");
+  }
+
+  while(i < length(cli)) {
+    argName = cli[i];
+
+    if (cli[i + 1] %=>% is.argName) {
+      # 下一个元素是命令参数名称
+      # 则当前的这个参数就是一个逻辑开关
+      args[argName] = TRUE;
+    } else {
+      args[argName] = cli[i + 1];
+      i = i + 1;
+    }
+  }
+
+  list(argv = cli, commandName = name, args = args);
+}
