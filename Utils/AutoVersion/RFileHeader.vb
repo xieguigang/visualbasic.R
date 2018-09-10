f0601afe348368e99a4e2e35a37642236f8a25f0
xieguigang <xie.guigang@gcmodeller.org>
2018-09-10 10:01:18 +0800

    add R script header

---
+++ Utils/AutoVersion/RFileHeader.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Text
+Imports System.Text.RegularExpressions
+Imports Microsoft.VisualBasic.Text
+
+Module RFileHeader
+
+    Const RFuncDeclare$ = "\S+\s*<-\s*function\(.+\)\s*\{"
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="r">脚本的文件路径</param>
+    ''' <returns></returns>
+    Public Function Summary(r As String) As String
+        Dim Rscript As String = r _
+            .ReadAllText _
+            .StringReplace("#Region ""Microsoft\.ROpen.+?"".+?#End Region", "", RegexOptions.Singleline) _
+            .Trim(" "c, ASCII.TAB, ASCII.CR, ASCII.LF)
+        Dim funcs = r.Matches(RFuncDeclare, RegexICSng) _
+                     .Select(Function(f)
+                                 Return f.LineTokens.JoinBy(" ")
+                             End Function) _
+                     .ToArray
+        Dim md5$ = r.MD5
+        Dim header As New StringBuilder
+
+        Call header.AppendLine($"#Region ""Microsoft.ROpen::{md5}, {r.FileName}""")
+        Call header.AppendLine()
+        Call header.AppendLine($"    # Summaries:")
+        Call header.AppendLine()
+
+        For Each func As String In funcs
+            Call header.AppendLine($"    # {func}")
+        Next
+
+        Call header.AppendLine()
+        Call header.AppendLine($"#End Region")
+
+        Return header.ToString
+    End Function
+End Module
