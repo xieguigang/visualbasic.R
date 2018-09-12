c2a11da5086aac70963bcc4b0dc157bb77b63968
xieguigang <xie.guigang@gcmodeller.org>
2018-09-12 15:52:29 +0800

    write test

---
+++ Rpackage/test/binarytree.R
@@ -0,0 +1,9 @@
+require(VisualBasic.R)
+
+src = c(1,1,1,2,3,400,5,6,5,6,7,88,99,0);
+tree <- binaryTree(src, function(x) x, function(x,y) x-y);
+
+node.is_leaf(tree[["14"]]);
+node.left(tree, tree[["6"]])
+node.find(tree, 400, function(x, y) x - y);
+node.find(tree, 0, function(x, y) x - y);
