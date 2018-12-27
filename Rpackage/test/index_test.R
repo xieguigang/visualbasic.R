c2a2d5ddf92ab5c1fada24e7ddb24e7277291217
xieguigang <xie.guigang@gcmodeller.org>
2018-12-27 08:44:27 +0800

    add index test

---
+++ Rpackage/test/index_test.R
@@ -0,0 +1,28 @@
+i <- sprintf("T%s", 1: 30000);
+
+index <- list();
+
+# 查找效率比较高，但是前面这里构建list的时候会比较慢
+for (x in i) {
+	index[[x]] <- 1;
+}
+
+
+x <- "T3000";
+
+test <- benchmark();
+
+for(j in 1:300000) {
+	dd <- x %in% i;
+}
+
+print(test());
+
+test <- benchmark();
+
+
+for (j in 1:300000) {
+	dd <- !is.null(index[[x]])
+}
+
+print(test());
