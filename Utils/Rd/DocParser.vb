181a1adedebd55f3ce93e4a2a1ca703c5123acb7
xieguigang <xie.guigang@gcmodeller.org>
2019-03-02 12:48:56 +0800

    add parser interface

---
+++ Utils/Rd/DocParser.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic.Emit.Marshal
+
+Module DocParser
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="rd">The text content of ``*.Rd`` file.</param>
+    ''' <returns></returns>
+    Public Function ParseDoc(rd As String) As RDoc
+        Dim text As New Pointer(Of Char)(rd.SolveStream)
+    End Function
+End Module
--- Utils/Rd/RDoc.vb
+++ Utils/Rd/RDoc.vb
@@ -10,6 +10,7 @@ Public Class RDoc
     Public Property usage As String
     Public Property arguments As Item()
     Public Property description As Doc
+    Public Property examples As String
 
 End Class
 
@@ -18,10 +19,64 @@ Public Class Item
     Public Property description As Doc
 End Class
 
-Public Class Doc
-
-End Class
-
 Public Class Enumerate
     Public Property items As Doc()
 End Class
+
+#Region "Doc content model"
+
+Public Class Doc
+
+    Public Property Fragments As DocFragment()
+
+    Public ReadOnly Property PlainText As String
+        Get
+            Return ToString()
+        End Get
+    End Property
+
+    Public Function GetMarkdown() As String
+
+    End Function
+
+    Public Function GetHtml() As String
+
+    End Function
+
+    Public Overrides Function ToString() As String
+        Return Fragments.Select(Function(frag) frag.ToString).JoinBy(" ")
+    End Function
+End Class
+
+Public MustInherit Class DocFragment
+
+End Class
+
+Public Class PlainText : Inherits DocFragment
+
+    Public Property text As String
+
+    Public Overrides Function ToString() As String
+        Return text
+    End Function
+End Class
+
+Public Class Code : Inherits DocFragment
+
+    Public Property content As DocFragment
+
+    Public Overrides Function ToString() As String
+        Return content.ToString
+    End Function
+End Class
+
+Public Class Link : Inherits DocFragment
+
+    Public Property target As String
+
+    Public Overrides Function ToString() As String
+        Return target.ToString
+    End Function
+End Class
+
+#End Region
