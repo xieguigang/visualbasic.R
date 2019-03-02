95e0f94cf7d48dae419d5e1f8f958666e6e9dc48
xieguigang <xie.guigang@gcmodeller.org>
2019-03-02 12:39:10 +0800

    add document class model

---
+++ Utils/Rd/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Rd")>
+<Assembly: AssemblyDescription("Document tools for R language")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Rd")>
+<Assembly: AssemblyCopyright("Copyright © xieguigang 2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2ff1b2c4-66e4-441b-aaf4-76366f74e8a2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ Utils/Rd/RDoc.vb
@@ -0,0 +1,27 @@
+﻿
+''' <summary>
+''' The R document model
+''' </summary>
+Public Class RDoc
+
+    Public Property name As String
+    Public Property [alias] As String
+    Public Property title As String
+    Public Property usage As String
+    Public Property arguments As Item()
+    Public Property description As Doc
+
+End Class
+
+Public Class Item
+    Public Property name As String
+    Public Property description As Doc
+End Class
+
+Public Class Doc
+
+End Class
+
+Public Class Enumerate
+    Public Property items As Doc()
+End Class
