68d20169e86de2f71b2ef51cd088be9b1c99a723
xieguigang <xie.guigang@gcmodeller.org>
2019-03-03 12:47:24 +0800

    parse content and write data

--- Utils/Rd/DocParser.vb
+++ Utils/Rd/DocParser.vb
@@ -1,26 +1,44 @@
 ﻿Imports Microsoft.VisualBasic.Emit.Marshal
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.DataFramework
 
-Public Class DocParser
+Public Class DocParser : Inherits RDocParser
 
-    Dim text As Pointer(Of Char)
-    Dim buffer As New List(Of Char)
     ''' <summary>
     ''' 标签栈的名称缓存
     ''' </summary>
     Dim nameBuffer As New List(Of Char)
     Dim parserEscape As New Escapes
-    Dim contentStack As New Stack(Of Char)
+    Dim doc As New RDoc With {
+        .comments = ""
+    }
 
-    ''' <summary>
-    ''' Rd文件之中的注释为单行注释
-    ''' </summary>
-    Dim docComments As String = ""
+    Dim textWriter As Dictionary(Of String, Action(Of String)) = GetType(RDoc) _
+        .GetProperties(PublicProperty) _
+        .Where(Function(p) p.PropertyType Is GetType(String)) _
+        .ToDictionary(Function(p) p.Name.ToLower,
+                      Function(p) As Action(Of String)
+                          Return Sub(text As String)
+                                     Call p.SetValue(doc, text)
+                                 End Sub
+                      End Function)
 
+    Dim contentWriter As Dictionary(Of String, Action(Of Doc)) = GetType(RDoc) _
+        .GetProperties(PublicProperty) _
+        .Where(Function(p) p.PropertyType Is GetType(Doc)) _
+        .ToDictionary(Function(p) p.Name,
+                      Function(p) As Action(Of Doc)
+                          Return Sub(doc As Doc)
+                                     Call p.SetValue(Me.doc, doc)
+                                 End Sub
+                      End Function)
 
     Public Class Escapes
 
+        ''' <summary>
+        ''' Rd文件之中的注释为单行注释
+        ''' </summary>
         Public docComment As Boolean
         Public stackOpen As Boolean
         Public stackNameParser As Boolean
@@ -36,9 +54,7 @@ Public Class DocParser
             Call walkChar(++text)
         Loop
 
-        Return New RDoc With {
-            .comments = docComments
-        }
+        Return doc
     End Function
 
     ''' <summary>
@@ -50,18 +66,30 @@ Public Class DocParser
         Return New DocParser(text:=New Pointer(Of Char)(rd.SolveStream)).CreateDoc
     End Function
 
-    Private Sub walkChar(c As Char)
+    Protected Overrides Sub walkChar(c As Char)
         If c = "%"c AndAlso buffer = 0 Then
             ' 以%符号起始，并且缓存为空，则说明是一条注释文本
             parserEscape.docComment = True
         ElseIf c = "{"c AndAlso parserEscape.stackNameParser Then
             ' 结束解析标签名称
-            parserEscape.stackNameParser = False
-            parserEscape.stackOpen = True
+            Dim stackName As New String(nameBuffer.PopAll)
+
+            Select Case stackName
+                Case "name", "alias", "usage", "examples"
+                    ' 纯文本内容
+                    textWriter(stackName)(New PlainTextParser(text).GetCurrentText)
+                Case "title", "details", "description"
+                    contentWriter(stackName)(New ContentParser(text).GetCurrentContent)
+                Case "arguments"
+
+                Case Else
+                    Throw New NotImplementedException(stackName)
+            End Select
+
         ElseIf c = ASCII.CR OrElse c = ASCII.LF Then
             If parserEscape.docComment Then
                 ' 单行注释
-                docComments = docComments & New String(buffer.PopAll)
+                doc.comments = doc.comments & New String(buffer.PopAll)
             End If
         ElseIf parserEscape.stackNameParser Then
             nameBuffer += c
@@ -75,13 +103,61 @@ Public Class DocParser
     End Sub
 End Class
 
-Public Class ContentParser
+Public MustInherit Class RDocParser
 
-    Sub New(text As Pointer(Of Char))
+    Protected text As Pointer(Of Char)
+    Protected buffer As New List(Of Char)
 
+    Protected MustOverride Sub walkChar(c As Char)
+
+End Class
+
+Public Class ContentParser : Inherits RDocParser
+
+    Friend Sub New(text As Pointer(Of Char))
+        Me.text = text
     End Sub
 
-    Public Function GetcurrentContent() As Doc
+    Protected Overrides Sub walkChar(c As Char)
+        Throw New NotImplementedException()
+    End Sub
 
+    ''' <summary>
+    ''' 可能包含有纯文本以及以下的标签：``\code{}``,``\link{}``,``\enumerate{}``
+    ''' </summary>
+    ''' <returns></returns>
+    Public Function GetCurrentContent() As Doc
+
+    End Function
+End Class
+
+Public Class PlainTextParser : Inherits RDocParser
+
+    ''' <summary>
+    ''' 因为R代码示例之中任然可能含有``{}``符号，所以会需要使用一个栈对象来确定文本结束的位置
+    ''' </summary>
+    Dim codeStack As New Stack(Of Char)
+
+    Friend Sub New(text As Pointer(Of Char))
+        Me.text = text
+        Me.codeStack.Push("{")
+    End Sub
+
+    Protected Overrides Sub walkChar(c As Char)
+        If c = "{"c Then
+            codeStack.Push("{"c)
+        ElseIf c = "}"c Then
+            codeStack.Pop()
+        End If
+
+        buffer += c
+    End Sub
+
+    Public Function GetCurrentText() As String
+        Do While codeStack.Count > 0
+            Call walkChar(++text)
+        Loop
+
+        Return New String(buffer)
     End Function
 End Class
