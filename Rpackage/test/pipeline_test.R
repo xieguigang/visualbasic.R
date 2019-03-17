1bb29865d4ebb13fabac28305d221154d5997685
xieguigang <xie.guigang@gcmodeller.org>
2019-03-17 19:31:04 +0800

    add pip test

--- Rpackage/test/pipeline_test.R
+++ Rpackage/test/pipeline_test.R
@@ -1,1 +1,30 @@
-"test.txt" %=>% ReadAllText %=>% File.Open("file_copy.txt"); 
+# "test.txt" %=>% ReadAllText %=>% File.Open("file_copy.txt"); 
+
+
+`>` <- function(x, y) {
+	if (is.function(y)) {
+		y(x)
+	} else {
+		sign( x - y) == 1;		
+	}
+}
+
+x = 999999;
+
+x 
+> is.na
+> is.null
+> print
+
+
+
+
+system.time({
+
+for(i in 1:1000000) {
+ i > c(999,24,2,342,34,23,423,432,442,32342,3,4);
+ 
+}
+
+
+})
