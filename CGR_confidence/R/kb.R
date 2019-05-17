75ec6df21e611dcb19cb15eb9b7dd80ca2a69e28
xieguigang <xie.guigang@gcmodeller.org>
2019-05-17 19:32:35 +0800

    outlier

--- CGR_confidence/R/kb.R
+++ CGR_confidence/R/kb.R
@@ -6,7 +6,38 @@
 #' @return A genotype data model
 #'
 measure.range <- function(samples) {
-   quartile <- quantile(samples);
+  data <- normal(samples %=>% quartile, samples);
+  average <- mean(data);
+  sd <-
 
+}
 
+quartile <- function(samples) {
+  q <- quantile(samples);
+  q <- list(
+    q1 = q[["25%"]],
+    q2 = q[["50%"]],
+    q3 = q[["75%"]],
+    IQR = q[["75%"]] - q[["25%"]],
+    min = min(samples),
+    max = max(samples)
+  );
+
+  q;
+}
+
+outlier <- function(quartile, samples) {
+  lower <- quartile$q1 - 1.5 * quartile$IQR;
+  upper <- quartile$q3 + 1.5 * quartile$IQR;
+  index <- (samples < lower) | (samples > upper);
+
+  samples[index];
+}
+
+normal <- function(quartile, samples) {
+  lower <- quartile$q1 - 1.5 * quartile$IQR;
+  upper <- quartile$q3 + 1.5 * quartile$IQR;
+  index <- (samples >= lower) & (samples <= upper);
+
+  samples[index];
 }
