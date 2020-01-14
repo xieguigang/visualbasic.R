11ebc982903d6dc6c5c5a6827601c6a040beacee
xieguigang <xie.guigang@gcmodeller.org>
2020-01-14 09:00:46 +0800

    Update slidewindow.R

---
+++ Rpackage/R/slidewindow.R
@@ -0,0 +1,18 @@
+slide.windows <- function(win_size = 2, step = 1, ...) {
+	data <- list(...);
+	keys <- names(data);
+	out <- list();
+	
+	for(i in seq(1, length(data[[1]]), by= step)) {
+		range <- i:(i + win_size -1);
+		element <- list();
+		
+		for(key in keys) {
+			element[[key]] <- data[[key]][range];
+		}
+		
+		out[[sprintf("X%s", i)]] <- element;
+	}
+	
+	out;
+}
