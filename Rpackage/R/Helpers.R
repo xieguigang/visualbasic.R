a996869d5d2ffcffe314ff37e2bd6b33bbbc593a
xieguigang <xie.guigang@gcmodeller.org>
2020-08-07 00:27:03 +0800

    get os

--- Rpackage/R/Helpers.R
+++ Rpackage/R/Helpers.R
@@ -209,3 +209,19 @@ alloca <- function(len, list = FALSE) {
     rep(NA, len = len);
   }
 }
+
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
