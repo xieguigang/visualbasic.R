d2b92864426a582dfa2c84c9aa5167e5a6d29c2b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-09-10 08:46:37 +0800

    add dev notes

--- dataUtils/R/table_reader.R
+++ dataUtils/R/table_reader.R
@@ -1,11 +1,15 @@
-read_table.auto <- function(path) {
+read_table.auto <- function(path, check.names = FALSE) {
   if (!isTsv(path)) {
-    read.csv(path);
+    read.csv(path, row.names = 1, header = T, check.names = check.names);
   } else {
-    read.table(path, header = TRUE, sep = "\t");
+    read.table(path, header = TRUE, sep = "\t", row.names = 1, check.names = check.names);
   }
 }
 
+#' determine file format from file name
+#'
+#' @param path tsv: \code{*.txt}, \code{*.xls}; csv: \code{*.csv}
+#'
 isTsv <- function(path) {
   file <- base::basename(path);
   out  <- strsplit(x=file, split="\\.", fixed=FALSE, perl=TRUE, useBytes=FALSE);
@@ -14,6 +18,14 @@ isTsv <- function(path) {
   ext != "csv";
 }
 
+#' Write table safely
+#'
+#' @param path create a file safely, create directory if the parent
+#'    directory is not exists, and determine file format by the file
+#'    name its extension name.
+#'
+#' @param data a \code{data.frame} object or matrix data object.
+#'
 write_table.auto <- function(data, path) {
   dir = dirname(path);
   dir.create(dir, recursive = TRUE);
