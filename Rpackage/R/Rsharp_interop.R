5fa5f10788d43dd46fed90ed1d3640f9a7e07737
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2021-04-15 17:48:11 +0800

    Update Rsharp_interop.R

--- Rpackage/R/Rsharp_interop.R
+++ Rpackage/R/Rsharp_interop.R
@@ -2,7 +2,11 @@ closureText = function(closure) {
 	closure = gsub("%>%", ":>", capture.output(closure), fixed = TRUE);
 	
 	if (closure[1] == "function() {") {
+		# write by hand
 		closure = closure[2:(length(closure) - 1)];
+	} else if (gsub("\\s", "", closure[1], perl = TRUE) == "function()") {
+		# formatted by R package compiler
+		closure = closure[3:(length(closure) - 3)];
 	}
 	
 	closure;
