4ae77ecd1edffa10bcf8a1387a2fd392b024c2cc
guigang xie <gg.xie@bionovogene.com>
2021-11-18 15:04:41 +0800

    parse commandline arguments as required

--- Rpackage/R/commandArgs.R
+++ Rpackage/R/commandArgs.R
@@ -1,10 +1,50 @@
-commandArgs = function(...) {
+commandArgs = function(..., debug = FALSE) {
     argv = list(...);
-    cmdl = base::commandArgs();
+    cmdl = VisualBasic.R::argv();
 
     if (length(argv) == 0) {
         return(cmdl);
+    } else if (debug) {
+        print("View of the input commandline arguments:");
+        str(cmdl);
+        print("commandline schema:");
+        str(argv);
     }
 
-    str(argv);
+    if (cmdl$hasArg("--help")) {
+        return(invisible(cmdl_helpDoc(argv)));
+    }
+
+    data = list();
+
+    # validation
+    # and get result string
+    for(argName in names(argv)) {
+        schema = argv[[argName]];
+        required = as.logical(schema[["required"]]);
+
+        if (!cmdl$hasArg(argName)) {
+            if (required) stop(schema[1]);
+
+            value = schema[1];
+        } else {
+            value = cmdl$nextToken(argName);
+        }
+
+        # string interpolation
+        value = cmdl_interpolate(value, cmdl);
+        argName = gsub("(^[/-]+)|([/-]+$)", "", argName);
+        argName = gsub("[-]+", ".", argName);
+        data[[argName]] = value;
+    }
+
+    data;
+}
+
+cmdl_helpDoc = function(argv) {
+    stop("not implemented!");
+}
+
+cmdl_interpolate = function(value, cmdl) {
+    value;
 }
