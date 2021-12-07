e1c484984caccb73ef2ce0e84106d703c14af774
guigang xie <gg.xie@bionovogene.com>
2021-12-07 09:38:36 +0800

    split source file

--- Rpackage/R/Enumerator.R
+++ Rpackage/R/Enumerator.R
@@ -1,17 +1,17 @@
 #Region "Microsoft.ROpen::f8fa48fb9540f25761f96b97a3477437, Enumerator.R"
 
-    # Summaries:
+# Summaries:
 
-    # Enumerator <- function(src) {...
-    # .select <- function(project) {...
-    # .where <- function(assert) {...
-    # .orderBy <- function(key, key.numeric = function(x) as.numeric(x)) {...
-    # .orderByDescending <- function(key, key.numeric = function(x) as.numeric(x)) {...
-    # .count <- function(predicate) {...
-    # Select= function(project) .select(project) %=>% Enumerator,Where = function(assert) .where(assert)%=>% Enumerator,OrderBy = function(key, key.numeric = as.numeric) {...
-    # OrderByDescending = function(key, key.numeric = as.numeric) {...
-    # ToArray = function() src,Take= function(n) Linq$Take(src, n)%=>% Enumerator,Skip= function(n) Linq$Skip(src, n)%=>% Enumerator,GroupBy = function(key, type) Linq$GroupBy(src, key, type) %=>% Enumerator,# Logical predicatesAny = function(predicate = NULL) {...
-    # All= function(predicate) {...
+# Enumerator <- function(src) {...
+# .select <- function(project) {...
+# .where <- function(assert) {...
+# .orderBy <- function(key, key.numeric = function(x) as.numeric(x)) {...
+# .orderByDescending <- function(key, key.numeric = function(x) as.numeric(x)) {...
+# .count <- function(predicate) {...
+# Select= function(project) .select(project) %=>% Enumerator,Where = function(assert) .where(assert)%=>% Enumerator,OrderBy = function(key, key.numeric = as.numeric) {...
+# OrderByDescending = function(key, key.numeric = as.numeric) {...
+# ToArray = function() src,Take= function(n) Linq$Take(src, n)%=>% Enumerator,Skip= function(n) Linq$Skip(src, n)%=>% Enumerator,GroupBy = function(key, type) Linq$GroupBy(src, key, type) %=>% Enumerator,# Logical predicatesAny = function(predicate = NULL) {...
+# All= function(predicate) {...
 
 #End Region
 
@@ -37,97 +37,97 @@ Enumerator <- function(src) {
   Imports("Microsoft.VisualBasic.Data");
   Imports("Microsoft.VisualBasic.Language");
 
-	type  <- GetType(src);
-	types <- primitiveTypes();
+  type  <- GetType(src);
+  types <- primitiveTypes();
   Linq  <- Microsoft.VisualBasic.Data.Linq()$methods;
 
-	#region "linq functions"
+  #region "linq functions"
 
-	.select <- function(project) {
-		if (type == types$data.frame) {
-			stop("Incompatible type!");
-		} else if (type == types$list) {
-			if (is.function(project)) {
-				lapply(src, project);
-			} else {
-				list.project(src, project);
-			}
-		} else if (type == types$vector) {
-			lapply(src, project);
-		} else {
-			stop("Incompatible type!");
-		}
-	}
+  .select <- function(project) {
+    if (type == types$data.frame) {
+      stop("Incompatible type!");
+    } else if (type == types$list) {
+      if (is.function(project)) {
+        lapply(src, project);
+      } else {
+        list.project(src, project);
+      }
+    } else if (type == types$vector) {
+      lapply(src, project);
+    } else {
+      stop("Incompatible type!");
+    }
+  }
 
-	.where <- function(assert) {
-		if (type == types$data.frame) {
-			stop("Incompatible type!");
-		} else if (type == types$list) {
+  .where <- function(assert) {
+    if (type == types$data.frame) {
+      stop("Incompatible type!");
+    } else if (type == types$list) {
 
-			names <- names(src)
-			test  <- sapply(names, function(name) assert(src[[name]])) %=>% which %=>% as.integer;
-			list  <- src[names[test]];
+      names <- names(src)
+      test  <- sapply(names, function(name) assert(src[[name]])) %=>% which %=>% as.integer;
+      list  <- src[names[test]];
 
-			list;
+      list;
 
-		} else if (type == types$vector) {
+    } else if (type == types$vector) {
 
-			test <- sapply(src, function(x) assert(x)) %=>% as.vector %=>% which %=>% as.integer;
-			list <- src[test];
+      test <- sapply(src, function(x) assert(x)) %=>% as.vector %=>% which %=>% as.integer;
+      list <- src[test];
 
-		} else {
-			stop("Incompatible type!");
-		}
-	}
+    } else {
+      stop("Incompatible type!");
+    }
+  }
 
-	#' Reorder the elements in the source sequence by a given \code{key}
-	#'
-	#' @param key Can be a string name which can select the property of
-	#'            the list object or a lambda function for evaluate the
-	#'            element object to numeric value.
-	.orderBy <- function(key, key.numeric = function(x) as.numeric(x)) {
+  #' Reorder the elements in the source sequence by a given \code{key}
+  #'
+  #' @param key Can be a string name which can select the property of
+  #'            the list object or a lambda function for evaluate the
+  #'            element object to numeric value.
+  .orderBy <- function(key, key.numeric = function(x) as.numeric(x)) {
     sort.list(src, key, key.numeric, FALSE);
-	}
+  }
 
-	.orderByDescending <- function(key, key.numeric = function(x) as.numeric(x)) {
-	  sort.list(src, key, key.numeric, TRUE);
-	}
+  .orderByDescending <- function(key, key.numeric = function(x) as.numeric(x)) {
+    sort.list(src, key, key.numeric, TRUE);
+  }
 
-	.count <- function(predicate) {
-	  if (predicate) {
-	    test <- sapply(src, predicate);
-	    sum(test);
-	  } else {
+  .count <- function(predicate) {
+    if (predicate) {
+      test <- sapply(src, predicate);
+      sum(test);
+    } else {
       length(src);
-	  }
-	}
+    }
+  }
 
-	#endregion
+  #endregion
 
-	list(src   = src,
-	   # source enumerator
-		 Select  = function(project) .select(project)               %=>% Enumerator,
-		 Where   = function(assert) .where(assert)                  %=>% Enumerator,
-		 OrderBy = function(key, key.numeric = as.numeric) {
-		    .orderBy(key, key.numeric)                              %=>% Enumerator
-		 },
-		 OrderByDescending = function(key, key.numeric = as.numeric) {
-		    .orderByDescending(key, key.numeric)                    %=>% Enumerator
-		 },
-		 ToArray = function() src,
-		 Take    = function(n) Linq$Take(src, n)                    %=>% Enumerator,
-		 Skip    = function(n) Linq$Skip(src, n)                    %=>% Enumerator,
-		 GroupBy = function(key, type) Linq$GroupBy(src, key, type) %=>% Enumerator,
-		 # Logical predicates
-		 Any     = function(predicate = NULL) {
-		    if (predicate) {
-		      Linq$Any(src, predicate);
-		    } else {
-		      !IsNothing(src);
-		    }
-		 },
-		 All     = function(predicate) {
-        .count() == .count(predicate);
-		 }
-	);
+  list(src   = src,
+       # source enumerator
+       Select  = function(project) .select(project)               %=>% Enumerator,
+       Where   = function(assert) .where(assert)                  %=>% Enumerator,
+       OrderBy = function(key, key.numeric = as.numeric) {
+         .orderBy(key, key.numeric)                              %=>% Enumerator
+       },
+       OrderByDescending = function(key, key.numeric = as.numeric) {
+         .orderByDescending(key, key.numeric)                    %=>% Enumerator
+       },
+       ToArray = function() src,
+       Take    = function(n) Linq$Take(src, n)                    %=>% Enumerator,
+       Skip    = function(n) Linq$Skip(src, n)                    %=>% Enumerator,
+       GroupBy = function(key, type) Linq$GroupBy(src, key, type) %=>% Enumerator,
+       # Logical predicates
+       Any     = function(predicate = NULL) {
+         if (predicate) {
+           Linq$Any(src, predicate);
+         } else {
+           !IsNothing(src);
+         }
+       },
+       All     = function(predicate) {
+         .count() == .count(predicate);
+       }
+  );
 }
