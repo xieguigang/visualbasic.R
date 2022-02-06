9bbae49187cf088068897841ceccedcc65ff2533
xieguigang <xie.guigang@gcmodeller.org>
2022-02-06 20:21:34 +0800

    coerce_listNode

--- Rpackage/R/Rsharp_interop.R
+++ Rpackage/R/Rsharp_interop.R
@@ -40,4 +40,46 @@ decode_rjson = function(json) {
 	if (is.character(json)) {
 		json = jsonlite::fromJSON(json);
 	}
+
+	if (is.list(json)) {
+		coerce_listNode(json); 
+	} else {
+		json;
+	}
+}
+
+coerce_listNode = function(node) {
+	if (is.list(node)) {
+		node = lapply(node, function(child) {
+			coerce_listNode(child); 
+		});
+
+		size_child = sapply(node, length);
+		type_child = sapply(node, function(child) {
+			if (is.matrix(child) || is.list(child) || is.data.frame(child)) {
+				FALSE;
+			} else {
+				TRUE;
+			}
+		});
+		all_equals = all(size_child == size_child[1]) & size_child[1] > 1;
+		all_primitive = all(type_child);
+
+		if (length(node) > 1 && all_equals && all_primitive) {
+			# convert to dataframe
+			tab = data.frame();
+
+			for(key in names(node)) {
+				tab = cbind(tab, node[[key]]);
+			}
+
+			colnames(tab) = names(node);
+
+			node = tab;
+		} else {
+			node;
+		}
+	} else {
+		node;
+	}
 }
