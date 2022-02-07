9ed6bdfa5599546a2bac53e322448508f71563cb
guigang xie <gg.xie@bionovogene.com>
2022-02-07 13:16:06 +0800

    build packages

--- Rpackage/R/Rsharp_interop.R
+++ Rpackage/R/Rsharp_interop.R
@@ -1,80 +1,80 @@
 #Region "Microsoft.ROpen::4c05eb3546997fcb339ec5c26e886e0a, Rsharp_interop.R"
 
-    # Summaries:
+# Summaries:
 
-    # closureText = function(closure) {...
+# closureText = function(closure) {...
 
 #End Region
 
 #' Translate R code as R# code
 #'
 closureText = function(closure) {
-	closure = gsub("%>%", ":>", capture.output(closure), fixed = TRUE);
-	trim    = function (x) sub("\\s+$", "", x)
+  closure = gsub("%>%", ":>", capture.output(closure), fixed = TRUE);
+  trim    = function (x) sub("\\s+$", "", x)
 
-	if (closure[1] == "function() {") {
-		# write by hand
-		closure = closure[2:(length(closure) - 1)];
-	} else if (gsub("\\s", "", closure[1], perl = TRUE) == "function()") {
-		# formatted by R package compiler
-		closure = closure[3:(length(closure) - 3)];
-	}
+  if (closure[1] == "function() {") {
+    # write by hand
+    closure = closure[2:(length(closure) - 1)];
+  } else if (gsub("\\s", "", closure[1], perl = TRUE) == "function()") {
+    # formatted by R package compiler
+    closure = closure[3:(length(closure) - 3)];
+  }
 
-	sapply(trim(closure), function(line) {
-		if (endsWith(line, ":>") ||
-			endsWith(line, "%do%") ||
-			endsWith(line, "%dopar%") ||
-			endsWith(line, "{") ||
-			endsWith(line, ",")) {
+  sapply(trim(closure), function(line) {
+    if (endsWith(line, ":>") ||
+        endsWith(line, "%do%") ||
+        endsWith(line, "%dopar%") ||
+        endsWith(line, "{") ||
+        endsWith(line, ",")) {
 
-			line;
-		} else {
-			sprintf("%s;", line);
-		}
-	}) %=>% as.vector;
+      line;
+    } else {
+      sprintf("%s;", line);
+    }
+  }) %=>% as.vector;
 }
 
 decode_rjson = function(json) {
-	require(jsonlite);
+  require(jsonlite);
 
-	if (is.character(json)) {
-		json = jsonlite::fromJSON(json);
-	}
+  if (is.character(json)) {
+    json = jsonlite::fromJSON(json);
+  }
 
-	if (is.list(json)) {
-		coerce_listNode(json); 
-	} else {
-		json;
-	}
+  if (is.list(json)) {
+    coerce_listNode(json);
+  } else {
+    json;
+  }
 }
 
 coerce_listNode = function(node) {
-	if (is.list(node)) {
-		node = lapply(node, function(child) {
-			coerce_listNode(child); 
-		});
+  if (is.list(node)) {
+    node = lapply(node, function(child) {
+      coerce_listNode(child);
+    });
 
-		size_child = sapply(node, length);
-		type_child = sapply(node, function(child) {
-			if (is.matrix(child) || is.list(child) || is.data.frame(child)) {
-				FALSE;
-			} else {
-				TRUE;
-			}
-		});
-		all_equals = all((size_child == size_child[1]) | size_child == 1) && any(size_child > 1);
-		all_primitive = all(type_child);
+    size_child = sapply(node, length);
+    type_child = sapply(node, function(child) {
+      if (is.matrix(child) || is.list(child) || is.data.frame(child)) {
+        FALSE;
+      } else {
+        TRUE;
+      }
+    });
+    all_equals = all((size_child == size_child[1]) | size_child == 1) && any(size_child > 1);
+    all_primitive = all(type_child);
 
-		if (length(node) > 1 && all_equals && all_primitive) {
-			# convert to dataframe
-			tab = data.frame(node);
-			colnames(tab) = names(node);
+    if (length(node) > 1 && all_equals && all_primitive) {
+      # convert to dataframe
+      tab = data.frame(node);
+      colnames(tab) = names(node);
 
-			node = tab;
-		} else {
-			node;
-		}
-	} else {
-		node;
-	}
+      node = tab;
+    } else {
+      node;
+    }
+  } else {
+    node;
+  }
 }
